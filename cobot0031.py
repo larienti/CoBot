0260869ac3cd70e828577608a052b3a64ee39db2
larienti <lars.arienti@gmail.com>
2023-01-09 21:01:25 -0500

    aaa

--- cobot0031.py
+++ cobot0031.py
@@ -2,8 +2,9 @@ from coinbase.wallet.client import Client
 import json, os
 
 api_key = os.getenv('API_KEY')
-client = Client(api_key)
-
+api_secret = os.getenv('API_SECRET')
+client = Client("api_key", "api_secret")
+print(api_key)
 accounts = client.get_accounts()
 for account in accounts.get("data"):
   print("Account ID: ", account.get("id"))
